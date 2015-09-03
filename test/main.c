ab6dfe4613e068518dd668e1b4a7827a32e801cf
Yufei Liu <yufeiliu94@gmail.com>
2015-09-03 20:33:22 +0800

    入口测试, 未实现

---
+++ test/main.c
@@ -0,0 +1,13 @@
+/**
+ * 主测试程序
+ * 并不用于测试功能
+ */
+
+#include <stdio.h>
+#include <include/bridge.h>
+
+int main(int args, char *argv[]) {
+    Bridge bridge;
+    bridge_instance(&bridge);
+    return 0;
+}
