8fcc6652f58973a7059c939e8cc239e0ff5bb006
Yufei Liu <yufeiliu94@gmail.com>
2015-09-04 11:13:33 +0800

    +REQUIRE语法糖

--- include/modloader.h
+++ include/modloader.h
@@ -6,10 +6,14 @@
 
 #include <include/module.h>
 #include <include/macro.h>
+#include <include/_modloader.h>
 
 typedef struct {
     ret_state (*require)(void **, const char *);
     ret_state (*remove)(const void *);
 } ModLoader;
+
+#define REQUIRE(type, name, mod) \
+    type * name; _bridge_modloader_require((void**)&name, mod)
 
 #endif //BRIDGE_MODLDR_H
--- test/modloader.c
+++ test/modloader.c
@@ -2,12 +2,11 @@
  * Bridge.ModLoader的测试程序
  */
 
-#include <include/_modloader.h>
+#include <include/modloader.h>
 #include <include/io.h>
 
 int main(int args, char *argv[]) {
-    ModIO * modio;
-    _bridge_modloader_require((void**)&modio, "Bridge.IO");
-    modio->write(1, "Hello, world!", 14);
+    REQUIRE(ModIO, io, "Bridge.IO");
+    io->write(1, "Hello, world!", 14);
     return 0;
 }
