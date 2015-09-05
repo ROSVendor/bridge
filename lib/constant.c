b0b70a118fb01cb1a724c4ace06c037783edb5e4
Yufei Liu <yufeiliu94@gmail.com>
2015-09-05 23:22:31 +0800

    +literal

---
+++ lib/constant.c
@@ -0,0 +1,30 @@
+/**
+ * Bridge在运行时使用的主要常量
+ */
+
+const char * family_names[] = {
+   "C",
+   "CPP",
+   "Haskell",
+   "Erlang",
+   "Python",
+   "Ruby",
+   "JavaScript",
+   "CommonLisp",
+   "Shcema",
+   "Rocket",
+   "Go",
+   "Java",
+};
+
+const char * runtime_names[] = {
+   "native",
+   "RTS",
+   "Beam",
+   "PVM",
+   "RVM",
+   "V8",
+   "Parrot",
+   "OpenJVM",
+   "OracleJVM",
+};
