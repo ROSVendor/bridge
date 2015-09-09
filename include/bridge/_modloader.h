571f3cb0475d5d167076bc271935585b4a2a5f86
Yufei Liu <yufeiliu94@gmail.com>
2015-09-09 14:32:50 +0800

    添加手动初始化/清理

--- include/bridge/_modloader.h
+++ include/bridge/_modloader.h
@@ -11,8 +11,38 @@
 // TODO 实现一个ANSI C的版本
 // TODO 添加静态扫描以配置足够的内存空间
 
+template <typename V>
+class Field {
+    public:
+    typedef V * content_ptr;
+    Field(auto name, record_t * record) : name(name), record(record) {};
+    inline content_ptr get(const char * key) {
+        return get(std::string(key));
+    }
+    inline content_ptr get(const std::string& key) {
+        record_t::const_iterator i = record->find(key);
+        if (i != record->end())
+            return i->second;
+        return nullptr;
+    }
+    inline void set(const char * key, content_ptr value) {
+        std::string s = key;
+        set(s, value);
+    }
+    inline void set(const std::string& key, content_ptr value) {
+        (*record)[key] = value;
+    }
+    inline void clear(auto key) {
+        set(key, nullptr);
+    }
+    private:
+    record_t *  record;
+    std::string name;
+};
+
 #ifdef __cplusplus
 extern "C" {
+
 #endif
     ret_state _bridge_modloader_require(void **, const char *);
     ret_state _bridge_modloader_replace(void **, const char *, const char *);
--- test/modloader.c
+++ test/modloader.c
@@ -2,11 +2,14 @@
  * Bridge.ModLoader的测试程序
  */
 
+#include <bridge/bridge.h>
 #include <bridge/modloader.h>
 #include <bridge/io.h>
 
 int main(int args, char *argv[]) {
+    bridge_initialize();
     REQUIRE(ModIO, io, "Bridge.IO");
     io->write(1, "Hello, world!", 14);
+    bridge_finalize();
     return 0;
 }
